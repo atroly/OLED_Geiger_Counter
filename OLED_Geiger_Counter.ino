7a0302a167c3520014ed2f2a3d8f32fbb8ae7956
Chris Taylor <25781272+atroly@users.noreply.github.com>
2018-07-30 00:33:32 +0100

    Add recording facility and max counts display

--- OLED_Geiger_Counter.ino
+++ OLED_Geiger_Counter.ino
@@ -19,6 +19,7 @@
 
 
 #include <Arduino.h>
+#include <EEPROM.h>
 #include <U8g2lib.h>
 
 #ifdef U8X8_HAVE_HW_SPI
@@ -28,9 +29,9 @@
 #include <Wire.h>
 #endif
 
-#define COUNT_SIZE 60
+#define COUNT_SIZE 10
 
-const float ver = 1.0;          //  firmware version
+const float ver = 1.1;          //  firmware version
 const int ledPin = LED_BUILTIN; //  output pin for LED
 const int batt_pin = A1;        //  input pin for the battery voltage
 const int modePin = 2;          //  input pin for the mode button
@@ -42,24 +43,42 @@ U8G2_SSD1306_128X64_NONAME_F_4W_SW_SPI u8g2(U8G2_R2, /* clock=*/ 12, /* data=*/ 
 
 // End of constructor list
 
-volatile unsigned int switch_mode=0; // current mode as set by panel switch
-int newmode=1;                       // flag set by switch interrupt
-volatile unsigned int count=0;       // number of geiger tube events since last reset
-char str[7];                         // holds string value to display
-int first_time=1;                    // special treatment of counts per minute until COUNT_SIZE is complete
-unsigned int per_sec[COUNT_SIZE];    // array for counts per minute calculation;
-unsigned int seconds=0;              // index into per_sec array
-unsigned int current_count=0;
+volatile unsigned int switch_mode = 0; // current mode as set by panel switch
+int newmode = 1;                       // flag set by switch interrupt
+volatile unsigned int count = 0;       // number of geiger tube events since last reset
+char str[12];                           // holds string value to display
+int first_time = 1;                    // special treatment of counts per minute until COUNT_SIZE is complete
+unsigned int per_sec[COUNT_SIZE];      // array for counts per minute calculation;
+unsigned int seconds = 0;              // index into per_sec array
+unsigned int current_count = 0;        // current display counts per second
+unsigned int max_count = 0;            // maximum recorded counts per second
+unsigned int eeprom_index = 0;         // index into EEPROM
 
 void setup(void) {
+  Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
   pinMode(modePin, INPUT_PULLUP);
   u8g2.begin();
+  Serial.println("Initialising counter");
+  for (int i = 0 ; i < (EEPROM.length() - 1) ; i++) {
+    if ((EEPROM.read(i) == 255) && (EEPROM.read(i+1) == 255)) {
+      eeprom_index = i;                 // set index counter to next EEPROM location
+      break;
+    }
+    sprintf(str, "%d %d", i, EEPROM.read(i)); // output saved values
+    Serial.println(str);
+  }
+  if (digitalRead(modePin) == LOW) {   // reset EEPROM index counter if button held on powerup
+    Serial.println("EEPROM reset");
+    display(str, "EEPROM reset");  
+    eeprom_index=0;   
+    delay(2000);
+  }
   dtostrf(ver, 6, 2, str); // arduino can't sprintf floats
   display(str, "Firmware Version");
   attachInterrupt(digitalPinToInterrupt(modePin), modeHandler, FALLING);
   attachInterrupt(digitalPinToInterrupt(countPin), countHandler, FALLING);
-  delay(1000);
+  delay(1000);                       // let user read firmware message
 }
 
 void loop(void) {
@@ -69,6 +88,10 @@ void loop(void) {
     if (switch_mode == 1)
       show_cps();
     if (switch_mode == 2)
+      show_max();
+    if (switch_mode == 3)
+      show_index();
+    if (switch_mode == 4)
       voltage();
     newmode=0;  
   }
@@ -77,7 +100,6 @@ void loop(void) {
 
 
 void show_cpm(void) {
-  static 
   int i;
   long total;
   
@@ -98,6 +120,16 @@ void show_cpm(void) {
 void show_cps(void) {
   sprintf(str, "%06d", current_count);
   display(str, "Counts per second");
+}
+
+void show_max(void) {
+  sprintf(str, "%06d", max_count);
+  display(str, "Maximum CPS");
+}
+
+void show_index(void) {
+  sprintf(str, "%06d", eeprom_index);
+  display(str, "EEPROM Index");
 }
 
 void voltage(void) {
@@ -139,7 +171,7 @@ long readVcc() {
 
 int update_check() {
   // check to see if it's time to update the count variables
-  // and flash keepalive LED
+  // and flash heartbeat LED
   // 
   static int ledState = LOW;
   static unsigned long previousMillis = 0;
@@ -149,6 +181,23 @@ int update_check() {
     previousMillis = currentMillis; // save last update time
     per_sec[seconds]=count;
     current_count=count;
+    if (count > max_count) {
+      max_count = count;
+    }
+    if (eeprom_index < EEPROM.length() - 2) {
+      if (count < 128) {
+        EEPROM.write(eeprom_index, (byte)count);
+        ++eeprom_index;
+      }
+      else {
+        EEPROM.write(eeprom_index, (byte)((count >> 8)) + 128);
+        ++eeprom_index;
+        EEPROM.write(eeprom_index, (byte)count);
+        ++eeprom_index;
+      }
+      EEPROM.write(eeprom_index, 255);
+      EEPROM.write(eeprom_index+1, 255);
+    }
     count=0;
     if (++seconds == COUNT_SIZE) {
       seconds=0;
@@ -159,7 +208,7 @@ int update_check() {
     } else {
       ledState = LOW;
     }
-    digitalWrite(ledPin, ledState);
+    digitalWrite(ledPin, ledState);  // heartbeat
     return 1;
   }
   return 0;
@@ -172,10 +221,10 @@ volatile unsigned long bounceTime=0; // ms count to debounce a pressed switch
 void modeHandler() {  
 // this is the interrupt handler for button presses
 // it ignores presses that occur in intervals less then the bounce time
-  if(millis() > bounceTime) {
+  if (millis() > bounceTime) {
     newmode = 1;
     ++switch_mode;
-    if (switch_mode > 2) {
+    if (switch_mode > 4) {
       switch_mode=0;
     }
     bounceTime = millis() + BOUNCE_DURATION;  // set whatever bounce time in ms is appropriate
